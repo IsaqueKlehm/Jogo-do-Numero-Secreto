6540faf8e8d4172604019e2ed4a7a208c4c50d62
Isaque Klehm <isaqueklehm@gmail.com>
2024-12-23 17:07:03 -0300

    projeto inicial

--- Sabor-Express/app.py
+++
@@ -1,9 +0,0 @@
-print('Sabor Express\n')
-
-print('1. Cadastrar Restaurante')
-print('2. Listar Restaurante')
-print('3. Ativar Restaurante')
-print('4. Sair\n')
-
-opcao_escolhida = input('Escolha uma opção: ')
-print('Você escolheu a opção', opcao_escolhida)
